3f41c62be8805c65cb08b879b2826b310872a902
fhc023 <fhc023@qq.com>
2014-10-29 16:37:24 +0800

    simplify

--- main.go
+++ main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
-  "fmt"
+  "net"
+  "bufio"
   )
 
+var sockets = make([]net.Conn, 0)
+
 func main() {
-  s, err := NewServer("25252")
+  ln, err := net.Listen("tcp", ":25252")
   if err != nil {
-		fmt.Println(err)
-	}
-  defer s.Close()
-  s.Listen()
+    return
+  }
+  defer ln.Close()
+
+  for {
+    conn, err := ln.Accept()
+    if err != nil {
+      continue
+    }
+    defer conn.Close()
+
+    sockets = append(sockets, conn)
+    go handleConnection(conn)
+  }
+}
+
+func handleConnection(conn net.Conn) {
+  reader := bufio.NewReader(conn)
+  for {
+    input, err := reader.ReadString('\n')
+    if err != nil {
+      for i, v := range sockets {
+        if v == conn {
+          sockets = append(sockets[:i], sockets[i + 1:]...)
+        }
+      }
+      continue
+    }
+    for _, v := range sockets {
+      if v == conn {
+        continue
+      }
+      v.Write([]byte(input))
+    }
+  }
 }
